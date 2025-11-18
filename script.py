572dc6e25561b52d7a95a31103abdb7df14904e9
Cutneckk <cutneckk@yandex.ru>
2025-11-18 18:43:41 +0300

    fix errors

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ x_pass, y_pass = random.randint(50, 400), random.randint(250, 260)
 # Координаты кнопки "Войти" всплывающего окна Steam о конфликте сессий.
 x_ent, y_ent = random.randint(40, 200), random.randint(340, 360)
 
-async def steam_login():
+async def steam_login(inf):
     while True:
         try:
             win = pygetwindow.getWindowsWithTitle('Войдите в аккаунт снова')[0]
@@ -18,12 +18,12 @@ async def steam_login():
             pyautogui.click(x=x_pass+win.left, y=y_pass+win.top)
             await asyncio.sleep(0.3)
 
-            pyautogui.write('d10Cjgdncu')
-            await asyncio.sleep(0.2)
-            pyautogui.write('1979')
-            await asyncio.sleep(0.3)
+            for i in inf:
+                pyautogui.write(i)
+                await asyncio.sleep(0.1)
 
             pyautogui.click(x=x_ent + win.left, y=y_ent + win.top)
+
             await asyncio.sleep(3)
             print('Вход выполнен!')
 
@@ -48,6 +48,7 @@ async def steam_login_alt(inf: str):
             await asyncio.sleep(0.3)
 
             pyautogui.click(x=x_ent + win.left, y=y_ent + win.top)
+
             await asyncio.sleep(3)
             print('Вход выполнен!')
 
@@ -60,7 +61,7 @@ async def main():
         inf = input('Вставьте пароль для автовхода: ')
         wei = input('Выберите способ автоматизации входа: ')
         if wei == '1':
-            await steam_login()
+            await steam_login(inf)
             break
         elif wei == '2':
             await steam_login_alt(inf)
